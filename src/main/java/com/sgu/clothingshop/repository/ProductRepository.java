f5a790dbcaa1c0ff76cb96738cc4d063d472d0eb
Tien Le <tien.le@zamo.io>
2018-11-18 14:39:28 +0700

    Show all products on Home page

--- src/main/java/com/sgu/clothingshop/repository/ProductRepository.java
+++ src/main/java/com/sgu/clothingshop/repository/ProductRepository.java
@@ -4,4 +4,6 @@ import com.sgu.clothingshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Iterable<Product> findAllByIsDeleted(Boolean isDelete);
 }
